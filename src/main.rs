2ecffbbee6bbd491acbf63052150ef0ea4780351
mirsella <mirsella@protonmail.com>
2023-12-02 21:09:28 +0100

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
-use std::{env, thread::sleep, time::Duration};
+use std::{
+    cell::Ref, env, fs::File, io::Write, os::unix::fs::FileExt, thread::sleep, time::Duration,
+};
 
 use anyhow::{anyhow, Context, Result};
 use dotenv_codegen::dotenv;
-use headless_chrome::{Browser, LaunchOptionsBuilder, Tab};
+use headless_chrome::{Browser, Element, LaunchOptionsBuilder, Tab};
 
 fn main() -> Result<()> {
     let user = dotenv!("insta_user");
@@ -26,40 +28,66 @@ fn main() -> Result<()> {
     let tab = browser.new_tab()?;
     tab.set_user_agent("Mozilla/5.0 (iPhone; CPU iPhone OS 17_1_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.1 Mobile/15E148 Safari/604.1", None, None)?;
     tab.navigate_to(&format!(
-        "https://www.instagram.com/accounts/login/?next=/p/{id}"
+        "https://www.instagram.com/accounts/login/?next=/p/{id}/comments"
     ))?
     .wait_until_navigated()?;
-    if let Ok(el) = tab.find_element("button[tabindex='0']") {
+    if let Ok(el) = tab.find_element_by_xpath(
+        "/html/body/div[6]/div[1]/div/div[2]/div/div/div/div/div[2]/div/div[3]/div[1]/div/button",
+    ) {
         println!("accepting cookies");
         el.click()?;
         tab.wait_until_navigated()?;
-        sleep(Duration::from_secs(1));
+        sleep(Duration::from_secs(2));
     }
     if tab.find_element("input[name=username]").is_ok() {
         logging_in(&tab, user, pass)?;
     }
     println!("logged in");
-    std::thread::park();
-    let scroll = tab.find_element_by_xpath("/html/body/div[2]/div/div/div[2]/div/div/div[1]/div[1]/div[2]/section/main/div/div[1]/div/div[2]/div/div[2]").context("scroll bar")?;
-    let scroll_classes = scroll.get_attribute_value("class")?.unwrap();
-    println!("scroll classes: {}", scroll_classes);
+    let scroll_xpath = "/html/body/div[2]/div/div/div[2]/div/div/div[1]/div[1]/div[2]/section/main/div/div/div/div[1]";
+    let jsscroll = format!("el = document.evaluate('{scroll_xpath}', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue; el.scrollTop = el.scrollHeight;");
     let loadingwheel = "div[data-visualcompletion=loading-state]";
-    let jsscroll = "let el = document.querySelector('');
-        el.scrollTop = el.scrollHeight;
-    ";
     loop {
-        tab.evaluate(jsscroll, true)?;
-        sleep(Duration::from_secs(1));
+        let mut quit = true;
+        _ = tab.find_element(loadingwheel).map(|_| quit = false);
+        tab.evaluate(&jsscroll, true)?;
+        _ = tab.find_element(loadingwheel).map(|_| quit = false);
+        sleep(Duration::from_millis(100));
+        _ = tab.find_element(loadingwheel).map(|_| quit = false);
+        if quit {
+            break;
+        }
     }
-    let comments_text =
-        tab.find_elements_by_xpath("//div/div/div[2]/div[1]/div[1]/div/div[2]/span")?;
-    println!("founds {} comments", comments_text.len());
-    for comments in comments_text {
-        let text = comments.get_inner_text()?;
-        println!("comment: {}", text);
-    }
-    std::thread::park();
+    let spans =
+        tab.find_elements_by_xpath("//div/div/div/div/div[2]/div/div[1]/div/div/span[2]/span[1]")?;
+    println!("founds {} comments", spans.len());
+    let comments = spans
+        .iter()
+        .map(Element::get_inner_text)
+        .collect::<Result<Vec<_>>>()
+        .unwrap();
+    write_comments_csv(id, comments.iter().map(AsRef::as_ref));
+    print_stats(&comments);
     Ok(())
+}
+
+fn print_stats(comments: &[String]) {
+    todo!()
+}
+
+fn write_comments_csv<'a, I: Iterator<Item = &'a str>>(id: &str, comments: I) {
+    let filename = format!("comments-{id}.csv");
+    let mut file = match File::create(filename.clone()) {
+        Ok(file) => file,
+        Err(e) => {
+            eprintln!("ERROR, open file {filename}: {e}");
+            return;
+        }
+    };
+    for comment in comments {
+        file.write_all(comment.as_bytes()).unwrap();
+        file.write_all(&[b',']).unwrap();
+    }
+    println!("wrote comments to {filename}");
 }
 
 fn logging_in(tab: &Tab, user: &str, pass: &str) -> Result<()> {
